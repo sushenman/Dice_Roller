8c2dc8ff4fd3da8acdd10cb5757beaeebd3a578c
sushenman <sushenman83@gmail.com>
2023-03-14 20:44:54 +0545

    inital commit

---
+++ app/src/main/java/com/example/dice_roller/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.dice_roller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}

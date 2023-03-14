60e5624b8c95ae62ccb5e960f6b76387bcfbce5f
sushenman <sushenman83@gmail.com>
2023-03-14 20:47:20 +0545

    lab

--- app/src/main/java/com/example/dice_roller/MainActivity.java
+++ app/src/main/java/com/example/dice_roller/MainActivity.java
@@ -12,13 +12,49 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private TextView textview;
+
+
+    class Dice
+    {
+        private int numSides;
+        private Random random;
+
+        public Dice(int numSides)
+        {
+            this.numSides = numSides;
+            random = new Random();
+        }
+        public int roll ()
+        {
+            return random.nextInt(numSides) + 1;
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.roll);
+        textview = findViewById(R.id.textView);
 
 
+        button.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View view)
+            {
+
+                Dice dice = new Dice(6);
+                int diceRoll = dice.roll();
+                textview.setText(Integer.toString(diceRoll));
+
+
+            }
+
+        });
 
 
     }
